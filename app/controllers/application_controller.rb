0e1849c5a584436878e6f5ea6e0094a8d30b7fac
sun-diane Sun <dianesun1@gmail.com>
2023-04-08 22:59:47 +0000

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  def square_blank_form
+    render({:template => "calculation_template/new_square_form.html.erb"})
+  end
 
+  def square_results
+    render()
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get("/square/new", { :controller => "application", :action => "square_blank_form" })
 
+  get("/square/results", { :controller => "application", :action => "square_results" })
+
+  #square_root / new
+
+  #payment / new
+
+  #random / new
 end
