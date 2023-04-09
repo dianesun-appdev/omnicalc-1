43b882604aa6f6fc4ac99bc34c4802e1f0d7d43b
sun-diane Sun <dianesun1@gmail.com>
2023-04-09 23:48:00 +0000

    Square calculator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   def square_blank_form
-    render({:template => "calculation_template/new_square_form.html.erb"})
+    render({ :template => "calculation_template/new_square_form.html.erb" })
   end
 
   def square_results
-    render()
+    render({ :template => "calculation_template/square_results.html.erb" })
   end
 end
